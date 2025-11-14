745e90c9daf237c6e1dab0751874e6c469b924ec
Juan Navarro <112259825+JuanNavarroQA@users.noreply.github.com>
2025-11-14 22:42:31 +0100

    Remove echo off from bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,3 +1,2 @@
-@echo off
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/JuanNavarroQA/bypassnro/refs/heads/main/unattend.xml || exit /b 1
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/JuanNavarroQA/bypassnro/refs/heads/main/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
